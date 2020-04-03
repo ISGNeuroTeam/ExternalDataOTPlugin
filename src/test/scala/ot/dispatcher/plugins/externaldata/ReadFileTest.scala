b2270381c71f6ff348d254d7c0fb91344eaecddc
andrey <astarchenkov@ot.ru>
2020-04-03 16:26:07 +0300

    Fix formats problems.

--- src/test/scala/ot/dispatcher/plugins/externaldata/ReadFileTest.scala
+++ src/test/scala/ot/dispatcher/plugins/externaldata/ReadFileTest.scala
@@ -9,15 +9,15 @@ import ot.dispatcher.sdk.test.CommandTest
 
 class ReadFileTest extends CommandTest {
   override val dataset: String = """[
-      |{"a":1, "b": 2},
-      |{"a":10, "b": 20}
+      |{"a":"1","b":"2"},
+      |{"a":"10","b":"20"}
       |]""".stripMargin
 
   test("Test 0. Command: | readFile parquet") {
     initialDf.show()
-    val path = new File("src/test/resources/temp/test_file_parquet").getAbsolutePath
+    val path = new File("src/test/resources/temp/read_test_file_parquet").getAbsolutePath
     initialDf.write.format("parquet").save(path)
-    val simpleQuery = SimpleQuery(""" format=parquet path=test_file_parquet """)
+    val simpleQuery = SimpleQuery(""" format=parquet path=read_test_file_parquet """)
     val commandReadFile = new ReadFile(simpleQuery, utils)
     val actual = execute(commandReadFile)
     val expected = dataset
@@ -26,9 +26,9 @@ class ReadFileTest extends CommandTest {
 
   test("Test 1. Command: | readFile json") {
     initialDf.show()
-    val path = new File("src/test/resources/temp/test_file_json").getAbsolutePath
-    initialDf.write.format("parquet").save(path)
-    val simpleQuery = SimpleQuery(""" format=parquet path=test_file_json """)
+    val path = new File("src/test/resources/temp/read_test_file_json").getAbsolutePath
+    initialDf.write.format("json").save(path)
+    val simpleQuery = SimpleQuery(""" format=json path=read_test_file_json """)
     val commandReadFile = new ReadFile(simpleQuery, utils)
     val actual = execute(commandReadFile)
     val expected = dataset
@@ -37,20 +37,9 @@ class ReadFileTest extends CommandTest {
 
   test("Test 2. Command: | readFile csv") {
     initialDf.show()
-    val path = new File("src/test/resources/temp/test_file_csv").getAbsolutePath
-    initialDf.write.format("parquet").save(path)
-    val simpleQuery = SimpleQuery(""" format=parquet path=test_file_csv """)
-    val commandReadFile = new ReadFile(simpleQuery, utils)
-    val actual = execute(commandReadFile)
-    val expected = dataset
-    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
-  }
-
-  test("Test 2. Command: | readFile avro") {
-    initialDf.show()
-    val path = new File("src/test/resources/temp/test_file_avro").getAbsolutePath
-    initialDf.write.format("parquet").save(path)
-    val simpleQuery = SimpleQuery(""" format=parquet path=test_file_avroww """)
+    val path = new File("src/test/resources/temp/read_test_file_csv").getAbsolutePath
+    initialDf.write.format("csv").option("header", "true").save(path)
+    val simpleQuery = SimpleQuery(""" format=csv path=read_test_file_csv """)
     val commandReadFile = new ReadFile(simpleQuery, utils)
     val actual = execute(commandReadFile)
     val expected = dataset
