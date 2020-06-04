e2c0754f38facbc825f9d68f473aa65626a7a5ce
Andrey Starchenkov <astarchenkov@isgneuro.com>
2020-06-04 12:39:07 +0300

    Add ignored test for SqlRead.

---
+++ src/test/scala/ot/dispatcher/plugins/externaldata/SqlReadTest.scala
@@ -0,0 +1,30 @@
+package ot.dispatcher.plugins.externaldata
+
+import org.apache.spark.sql.SparkSession
+import org.scalatest.Ignore
+import ot.dispatcher.plugins.externaldata.commands.SQLRead
+import ot.dispatcher.sdk.core.SimpleQuery
+import ot.dispatcher.sdk.test.CommandTest
+
+@Ignore
+class SqlReadTest extends CommandTest {
+  override val dataset: String = ""
+
+  override val spark: SparkSession = SparkSession.builder()
+    .appName("Small plugin Tests")
+    .master("local")
+    .config("spark.sql.files.ignoreCorruptFiles", value = true)
+    .config("spark.driver.extraClassPath", "/home/andrey/SpeedDir/projects/IdeaProjects/ExternalDataOTPlugin/src/test/resources/postgresql-42.2.5.jar")
+    .getOrCreate()
+
+
+  test("Read from Postgresql DB.") {
+    val simpleQuery = SimpleQuery(""" base=postgres host=localhost user=dispatcher password=P@$$w0rd db=db query="SELECT 1 as field" """)
+    val commandSQLRead = new SQLRead(simpleQuery, utils)
+    val actual = execute(commandSQLRead)
+    val expected = dataset
+    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
+
+  }
+
+}
